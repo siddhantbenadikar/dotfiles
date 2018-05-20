b1f9cf042be4e9b88c87c4e051b37a645768c17c
siddhantbenadikar <siddhantn@hotmail.com>
2018-05-20 01:08:32 -0400

    added setup.sh to automate new mac setup

---
+++ setmeup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+set -e
+
+#Install Homebrew if not present
+tput setaf 2; echo 'Installing brew...'
+which -s brew
+if [[ $? != 0 ]] ; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+#Install zsh if not present
+tput setaf 2; echo 'Installing zsh...'
+which -s zsh
+if [[ $? != 0 ]] ; then
+	brew install zsh
+else 
+    tput setaf 2; echo "You already have zsh...skipping"
+fi
+
+#Install oh-my-zsh
+tput setaf 2; echo 'Installing oh-my-zsh...'
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Install figlet, cowsay, lolcat, fortune
+tput setaf 2; echo 'Installing figlet, cowsay, lolcat, fortune'
+brew install cowsay
+brew install lolcat
+brew install figlet
+brew install fortune
+
+#Setting up .vimrc and .zshrc
+tput setaf 2; echo 'Setting up .vimrc and .zshrc...'
+cp .vimrc ~/ &&
+cp .zshrc ~/
+
+#Install zsh-custom and zsh-syntax-highlighting
+tput setaf 2; echo 'Install zsh-custom and zsh-syntax-highlighting...'
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &
+wait
+
+tput setaf 2; echo 'DONE!!!'
+tput setaf 1; echo 'You need to download and setup cobalt-2 manually to complete the setup process.'
+
+
+
+
+
+
+
+
